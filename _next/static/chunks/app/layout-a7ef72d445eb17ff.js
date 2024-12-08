7e2b4521074f4671fb42ac1de5ec41b669a8f046
Jera <jerameelperezm@gmail.com>
2024-12-07 21:16:57 -0500

    Subiendo carpeta out para GitHub Pages

---
+++ _next/static/chunks/app/layout-a7ef72d445eb17ff.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{484:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,5356,23)),Promise.resolve().then(l.t.bind(l,7995,23))},7995:()=>{},5356:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_b5c575"}}},e=>{var s=s=>e(e.s=s);e.O(0,[508,441,517,358],()=>s(484)),_N_E=e.O()}]);
